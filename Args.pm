f3e4d02c3b59339dfb12a7f78d307b8bf69a23f2
Ilya Grigoriev <iliagrigoryev@yandex.ru>
2024-09-28 14:45:42 +0300

    feat: added printing languages code

--- Args.pm
+++ Args.pm
@@ -1,19 +1,35 @@
 package Args;
 
 our $text_request;
+our @langs = ("en", "ru");
 my $is_examples = 0;
+
 sub check_args {
+    my $ind = 0;
 	foreach (@ARGV) {
 		if ($_ eq "-e") {
 			$is_examples = 1;
 		}
+        elsif ($_ eq "-pl" || $_ eq "--print-langs") {
+            Request::print_supported_languages();
+            exit(0);
+        }
+        elsif ($_ eq "--lang") {
+            @langs = split(/-/, $ARGV[$ind + 1]);
+            if (@langs[0] eq "" || @langs[1] eq "") {
+                print STDERR "problem with setting languages\n";
+                exit(1);
+            }
+        }
 		else {
 			$text_request = $_;
 		}
+
+        $ind++;
 	}
 
 	if ($text_request eq "") {
-		print STDERR "no text input";
+		print STDERR "no text input\n";
 		exit(1);
 	}
 };
--- Request.pm
+++ Request.pm
@@ -2,43 +2,56 @@ package Request;
 
 sub request_get_translating {
 	my $text = shift;
-	my $cookie_yu = shift;
-	my $cookie_sprvk = shift;
+    my @cookies = shift;
+    my @langs = @Args::langs;
 	my $uuid = `uuidgen -t | tr -d - | tr -d "\n"`;
 
-	return `curl -s -m 3 'https://translate.yandex.net/api/v1/tr.json/translate?id=$uuid-0-0&srv=tr-text&source_lang=en&target_lang=ru&reason=cut&format=text&strategy=0&disable_cache=false&ajax=1&yu=$cookie_yu&sprvk=$cookie_sprvk%3D' \\
+	return `curl -s -m 4 'https://translate.yandex.net/api/v1/tr.json/translate?id=$uuid-0-0&srv=tr-text&source_lang=$langs[0]&target_lang=$langs[1]&reason=cut&format=text&strategy=0&disable_cache=false&ajax=1&yu=$cookies[0]&sprvk=$cookies[1]%3D' \\
 	-H 'accept: */*' \\
-	-H 'accept-language: en-US,en;q=0.9' \\
+	-H 'accept-language: en-US,en;q=1.9' \\
 	-H 'content-type: application/x-www-form-urlencoded' \\
 	-H 'origin: https://translate.yandex.ru' \\
-	-H 'priority: u=1, i' \\
+	-H 'priority: u=2, i' \\
 	-H 'referer: https://translate.yandex.ru/' \\
-	-H 'sec-ch-ua: "Chromium";v="129", "Not=A?Brand";v="8"' \\
-	-H 'sec-ch-ua-mobile: ?0' \\
+	-H 'sec-ch-ua: "Chromium";v="130", "Not=A?Brand";v="8"' \\
+	-H 'sec-ch-ua-mobile: ?1' \\
 	-H 'sec-ch-ua-platform: "Linux"' \\
 	-H 'sec-fetch-dest: empty' \\
 	-H 'sec-fetch-mode: cors' \\
 	-H 'sec-fetch-site: cross-site' \\
-	-H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36' \\
+	-H 'user-agent: Mozilla/6.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36' \\
 	-H 'x-retpath-y: https://translate.yandex.ru' \\
-	--data-raw 'text=$text&options=4'`;
+	--data-raw 'text=$text&options=5'`;
 };
 
 sub request_complete_translating {
 	my $text = shift;
-	return `curl -m 3 'https://predictor.yandex.net/suggest.json/complete?q=$text&srv=tr-text&lang=en&limit=5' 2>&1`;
+	return `curl -m 4 'https://predictor.yandex.net/suggest.json/complete?q=$text&srv=tr-text&lang=en&limit=5' 2>&1`;
 };
 
 sub get_cookies {
 	my $cookies = `curl -s -o /dev/null https://translate.yandex.ru/ -c -`;
 
 	$cookies =~ /yandexuid\t((\d)*)/;
-	my $cookie_yu = $1;
+	my $cookie_yu = $2;
 
 	$cookies =~ /spravka\t((?:(?!(=)).)*)/;
-	my $cookie_sprvk = $1;
+	my $cookie_sprvk = $2;
 
 	return ($cookie_yu, $cookie_sprvk);
-}
+};
 
-1;
+sub print_supported_languages {
+    my @langs = `curl -s https://www.loc.gov/standards/iso639-2/php/code_list.php | tr -d '\t\n\015' | grep -oP '<tr valign="top">(.*)<\/tr>'`;
+    foreach (@langs) {
+        my @matches = ($_ =~ /<td>([\w]{2})<\/td><td>([\w\s;,]*)<\/td>/g);
+
+        my $ind = 0;
+        while ($ind < scalar @matches) {
+            print "$matches[$ind] -- $matches[$ind + 1]\n";
+            $ind += 2;
+        }
+    }
+};
+
+2;
